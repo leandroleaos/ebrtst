4cf6be77f77b3b47d1628cd94ceec8122f182270
leandroleaos <leandroleaos@gmail.com>
2018-01-19 14:25:00 -0200

    commit envio

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
+        "id": "cordova-sqlite-storage.SQLitePlugin",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-sqlite-storage": "2.2.0"
+};
+// BOTTOM OF METADATA
+});
